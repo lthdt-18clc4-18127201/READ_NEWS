decb4f7fe2795ed975d5d4db5f12136b45c69c86
w1oz <47582211+w1oz@users.noreply.github.com>
2021-01-10 14:22:15 +0700

    upload image

--- Final-project/src/app/controllers/NewsController.js
+++ Final-project/src/app/controllers/NewsController.js
@@ -2,7 +2,8 @@ const { render } = require("node-sass");
 const Post = require('../Models/Post');
 const { mutipleMongooseToObject } = require('../../util/mongoose');
 const { mongooseToObject } = require('../../util/mongoose');
-
+var fs = require('fs');
+var path = require('path');
 class NewsController {
     async news(req, res, next) {
     //console.log(req.user.name)
@@ -49,7 +50,8 @@ class NewsController {
     };
 
     store(req, res, next) {
-        
+        var a=fs.readFileSync(path.join(__dirname,'../../../uploads/',req.file.filename)).toString('base64')
+        req.body.img='data:image/jpeg;base64,'+a
         const new_add = new Post(req.body);
         new_add.save();
         res.redirect('/me/stored/news');
@@ -66,12 +68,17 @@ class NewsController {
         }
 
         Post.findById(req.params.id)
+        
             .then(post => res.render('news/edit', { post: mongooseToObject(post),user: req.user,check,check_role }))
             .catch(next)
     };
 
     // [PUT] /news/:id
     update(req, res, next) {
+        
+        //console.log(req.img)
+        var a=fs.readFileSync(path.join(__dirname,'../../../uploads/',req.file.filename)).toString('base64')
+        req.body.img='data:image/jpeg;base64,'+a
         Post.updateOne({ _id: req.params.id}, req.body)
             .then(() => res.redirect('/me/stored/news'))
             .catch(next);
--- Final-project/src/routes/news.js
+++ Final-project/src/routes/news.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const newsController = require('../app/controllers/NewsController');
+const multer  = require('multer')
+const { update } = require('../app/controllers/NewsController');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+var upload = multer({ storage: storage });
+
 
 router.get('/add', newsController.add);
-router.post('/store', newsController.store);
+router.post('/store', upload.single('img'), newsController.store);
 router.get('/:id/edit', newsController.edit);
-router.put('/:id', newsController.update);
+router.put('/:id', upload.single('img'),newsController.update);
 router.delete('/:id', newsController.delete);
 router.delete('/:id/force', newsController.forcedelete);
 router.patch('/:id/restore', newsController.restore);
